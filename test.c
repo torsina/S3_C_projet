5e17db7ebd552e85d7f945e4189473fb502746e7
torsina <theo.sagot.1@gmail.com>
2019-11-18 11:13:53 +0100

    test

---
+++ test.c
@@ -0,0 +1,9 @@
+//
+// Created by torsina on 18/11/2019.
+//
+
+#include "test.h"
+
+int foo() {
+    return 5;
+}
---
+++ test.h
@@ -0,0 +1,10 @@
+//
+// Created by torsina on 18/11/2019.
+//
+
+#ifndef PROJET_TEST_H
+#define PROJET_TEST_H
+
+int foo();
+
+#endif //PROJET_TEST_H
