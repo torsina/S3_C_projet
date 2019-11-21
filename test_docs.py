2cec85ba16eae3297f5f656c24af9df259d92084
torsina <theo.sagot.1@gmail.com>
2019-11-21 10:34:21 +0100

    action-build-docs (#19)
    * add docs build test and a script

---
+++ test_docs.py
@@ -0,0 +1,22 @@
+import subprocess
+import platform
+import sys
+
+_dir = ""
+_make = ""
+if platform.system() == "Linux":
+    _dir = "../build"
+    _make = "make"
+else:
+    _dir = "./cmake-build-debug"
+    _make = "mingw32-make"
+
+pipes = subprocess.Popen([_make, "docs"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=_dir)
+std_out, std_err = pipes.communicate()
+
+print(str(std_out, 'utf-8'))
+print("")
+if len(std_err) != 0 and " WARNING: " in str(std_err):
+    print(str(std_err, 'utf-8'))
+    sys.exit(-1)
+sys.exit(0)
