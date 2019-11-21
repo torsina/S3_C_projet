904acd6ef2db79c3c76b6ca6ab2bc6ad404e66b3
torsina <theo.sagot.1@gmail.com>
2019-11-21 09:59:39 +0100

    switch to array for command

--- test_docs.py
+++ test_docs.py
@@ -8,7 +8,7 @@ if platform.system() == "Linux":
 else:
     _dir = "./cmake-build-debug"
 
-pipes = subprocess.Popen("make docs", stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=_dir)
+pipes = subprocess.Popen(["make", "docs"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=_dir)
 std_out, std_err = pipes.communicate()
 
 print(std_out)
