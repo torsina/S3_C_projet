77fab41b7767d6b5a3b87b8eb8094254b5c1edbd
torsina <theo.sagot.1@gmail.com>
2019-12-17 01:42:03 +0100

    docs for evaluate

--- src/sudoku/includes/sudoku.h
+++ src/sudoku/includes/sudoku.h
@@ -7,6 +7,23 @@ typedef struct _Sudoku Sudoku;
 
 unsigned int sudoku_get_dim_size(Sudoku* sudoku);
 
+/**
+ * \brief creates the score this sulution got on the sudoku.
+ *
+ * This function create the score of a solution to the sudoku
+ * by counting the duplicates inside each row and column,
+ * adding the counts together and substracting that to the square of the number
+ * of values in the sudoku.
+ *
+ * \author Group 14
+ * \version 0.0.1
+ * \date 2019
+ * \fn unsigned int evaluate(unsigned int* individual, const void* sudoku)
+ * \param individual a pointer to the solution proposed for the problem.
+ * \param sudoku a pointer to the Sudoku to solve.
+ * \sa ga_population_next
+ */
+
 extern unsigned int evaluate(unsigned int* individual, const void* sudoku);
 
 #endif  // SRC_SUDOKU_INCLUDES_SUDOKU_H_
