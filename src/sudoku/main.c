32d7426454cdcff22a156a49054ab14b250d955b
torsina <theo.sagot.1@gmail.com>
2019-12-17 01:35:04 +0100

    evaluate function

--- src/sudoku/main.c
+++ src/sudoku/main.c
@@ -14,55 +14,84 @@ unsigned int sudoku_get_dim_size(Sudoku* sudoku) {
   return sudoku->dim_size;
 }
 
-unsigned int evaluate(unsigned int* individual, const void* sudoku) {
-  sudoku = (Sudoku*)sudoku;
-  unsigned int score = 0;
-  // keep a state of every column and row already checked
-  unsigned int dim_size = sudoku_get_dim_size(sudoku);
-  bool state_rows = ga_malloc(dim_size);
-  bool state_columns = ga_malloc(dim_size);
-  // read left to right
-  unsigned int index = 0;
-  while(index < pow(dim_size, 2)) {
-    index++;
-  }
-}
-
 unsigned int* _evaluate_merge_problem_solution(unsigned int* individual, Sudoku* sudoku) {
   size_t size = pow(sudoku_get_dim_size(sudoku), 2);
   unsigned int* merge = ga_malloc(sizeof(unsigned int) * size);
   for(unsigned int i = 0; i < size; i++) {
-    if(sudoku->problem[i] != NULL) merge[i] = (unsigned int)sudoku->problem[i];
-    else merge[i] = individual[i];
+    if(sudoku->problem[i] != 0) merge[i] = (unsigned int)sudoku->problem[i];
+    else merge[i] = individual[i] + 1;
   }
   return merge;
 }
 
-bool _evaluate_check_row(unsigned int* individual, Sudoku* sudoku, unsigned int index) {
+
+unsigned int _evaluate_used_numbers_duplicates(unsigned int* used_values, unsigned int used_size) {
+  unsigned int unique_values[used_size];
+  unsigned int unique_size = 0;
+  unsigned int duplicates = 0;
+  for(unsigned int i = 0; i < used_size; i++) {
+    bool result = true;
+    unsigned int j = 0;
+    while(j < unique_size && result) {
+      if(unique_values[j] == used_values[i]) result = false;
+      else unique_values[unique_size] = used_values[i];
+      j++;
+    }
+    if(!result) duplicates++;
+  }
+  return duplicates;
+}
+
+
+unsigned int _evaluate_check_row(unsigned int* merge, Sudoku* sudoku, unsigned int index) {
   unsigned int dim_size = sudoku_get_dim_size(sudoku);
   unsigned int rowIndex = index / dim_size;
   unsigned int start = rowIndex * dim_size;
   unsigned int end = rowIndex * (dim_size + 1);
   // max int for a value = dim_size - 1
-  unsigned int used_values = malloc(sizeof(unsigned int) * (dim_size - 1));
-  unsigned int i = start;
-  bool result = true;
-  while(i < end && result) {
-    i++;
+  unsigned int* used_values = malloc(sizeof(unsigned int) * (dim_size - 1));
+  unsigned int used_size = 0;
+  for(unsigned int i = start; i < end; i++) {
+    used_values[used_size] = merge[i];
+    used_size++;
   }
+  unsigned int duplicates = _evaluate_used_numbers_duplicates(used_values, used_size);
+  free(used_values);
+  return duplicates;
 }
 
-bool _evaluate_check_column() {
-
-}
-
-bool _evaluate_add_used_number(unsigned int* used_values, unsigned int used_size, unsigned int new_value) {
-  unsigned int i = 0;
-  bool result = true;
-  while(i < used_size && result) {
-    if(used_values[i] == new_value) result = false;
-    i++;
+bool _evaluate_check_column(unsigned int* merge, Sudoku* sudoku, unsigned int index) {
+  unsigned int dim_size = sudoku_get_dim_size(sudoku);
+  unsigned int columnIndex = index % dim_size;
+  // max int for a value = dim_size - 1
+  unsigned int* used_values = malloc(sizeof(unsigned int) * (dim_size - 1));
+  unsigned int used_size = 0;
+  for(unsigned int i = columnIndex; i < pow(dim_size, 2); i += dim_size) {
+    used_values[used_size] = merge[i];
+    used_size++;
   }
-  used_values[used_size] = new_value;
-  return result;
+  unsigned int duplicates = _evaluate_used_numbers_duplicates(used_values, used_size);
+  free(used_values);
+  return duplicates;
 }
+
+unsigned int evaluate(unsigned int* individual, const void* sudoku) {
+  sudoku = (Sudoku*)sudoku;
+  unsigned int dim_size = sudoku_get_dim_size(sudoku);
+  unsigned int score = pow(dim_size, 2);
+  unsigned int* merge = _evaluate_merge_problem_solution(individual, sudoku);
+  unsigned int duplicates = 0;
+  // rows
+  for(unsigned int i = 0; i < pow(dim_size, 2); i += dim_size) {
+    duplicates += _evaluate_check_row(merge, sudoku, i);
+  }
+  // columns
+  for(unsigned int i = 0; i < dim_size; i++) {
+    duplicates += _evaluate_check_column(merge, sudoku, i);
+  }
+  free(merge);
+  // afraid that score could be negative if too much duplicates(because an error can be counted multiple times)
+  // so squaring score to be sure
+  return pow(score, 2) - duplicates;
+}
+
---
+++ test/sudoku/test-evaluate.c
@@ -0,0 +1,18 @@
+//
+// Created by Aelis on 17/12/2019.
+//
+
+#include <stdlib.h>
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <assert.h>
+
+#include "../../src/sudoku/includes/sudoku.h"
+#include "../../src/sudoku/sudoku.inc"
+
+int main(void) {
+
+  return EXIT_SUCCESS;
+}
