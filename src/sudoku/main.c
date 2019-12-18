b4ef982f7ea076d84c398c3e58d5c321a1456946
T-MMLR <tim.godreuil@etudiant.univ-lr.fr>
2019-12-18 16:41:12 +0100

    Linked libYAML

--- src/sudoku/main.c
+++ src/sudoku/main.c
@@ -7,8 +7,6 @@
 
 #include "sudoku.inc"
 int main(void) {
-  Sudoku* test = malloc(sizeof(Sudoku));
-  sudoku_get_dim_size(test);
   printf("libYAML version : %s\n", yaml_get_version_string());
   return EXIT_SUCCESS;
 }
